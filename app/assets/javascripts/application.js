e00bf501d0f4abb3f9d2a0b0a5db6b01b42106d7
vrrodgers <trulymissvicky1982@gmail.com>
2019-01-22 18:02:11 -0600

    fixed spacing for gallery images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,7 @@
 $('.carousel').carousel()
 
 document.addEventListener("turbolinks:load", function () {
+    
   function events(){
     var days = document.getElementsByClassName('day');
     
@@ -38,16 +39,54 @@ document.addEventListener("turbolinks:load", function () {
       var dayCheck = check.trim();
       console.log(upcoming);
       if(upcoming.includes(dayCheck)){
-          days[i].style.background = "yellow"; 
+          days[i].style.backgroundImage = 'url(https://s3-us-west-2.amazonaws.com/v-daycare/uploads/gallery/image/bg_td-upcoming.png)';
       }
       if(archived.includes(dayCheck)){
-          days[i].style.background = "pink"; 
+          days[i].style.backgroundImage = 'url(https://s3-us-west-2.amazonaws.com/v-daycare/uploads/gallery/image/bg_td-archival.png)';
       }
     }
   };
   events();
 
 })
+
+$(function () {
+
+    var $container = $('#masonry-container');
+
+    $container.imagesLoaded(function () {
+        $container.masonry({
+            itemSelector: '.box',
+            columnWidth: 100
+        });
+    });
+
+    $container.infinitescroll({
+            navSelector: '#page-nav', // selector for the paged navigation
+            nextSelector: '#page-nav a', // selector for the NEXT link (to page 2)
+            itemSelector: '.box', // selector for all items you'll retrieve
+            loading: {
+                finishedMsg: 'No more pages to load.',
+                img: 'http://i.imgur.com/6RMhx.gif'
+            }
+        },
+        // trigger Masonry as a callback
+        function (newElements) {
+            // hide new items while they are loading
+            var $newElems = $(newElements).css({
+                opacity: 0
+            });
+            // ensure that images load before adding to masonry layout
+            $newElems.imagesLoaded(function () {
+                // show elems now they're ready
+                $newElems.animate({
+                    opacity: 1
+                });
+                $container.masonry('appended', $newElems, true);
+            });
+        }
+    );
+});
 
 
 
